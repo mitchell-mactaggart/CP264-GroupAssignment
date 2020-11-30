1b71b1bd6bf352c375ec450100b7697ccb2957b9
mitchell-mactaggart <69057561+mitchell-mactaggart@users.noreply.github.com>
2020-11-30 13:23:14 -0500

    main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,17 @@
+int main() {
 
+	FILE *originalFile; // 2 files, the original message and the encoded message
+
+	unsigned int numChars, frequencies[NUM_CHARS];
+
+	// Open the original file
+	originalFile = fopen("test.txt", "r");
+
+	// Read the file and determine the frequencies
+	numChars = get_freq(originalFile, frequencies);
+	printf("test");
+	printf("ch: %c", numChars);
+	fclose(originalFile);
+	printf("test");
+
+}
